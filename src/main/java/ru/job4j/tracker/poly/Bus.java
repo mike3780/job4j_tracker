3950bf86be6fe8d895fb570b1d7fa596c1a8671e
Mihail Treschalin <mihail.treschalin@ya.ru>
2022-05-15 01:28:33 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("The bus is coming");
+    }
+
+    @Override
+    public void passengers(int passengers) {
+        System.out.println("In the bus " + passengers);
+    }
+
+    @Override
+    public float refuel(float fuel) {
+        float fuelCoast = 1000;
+        return fuelCoast;
+    }
+}
