96b610ba31a771b06da182fbf92d49a9c18e1588
Mihail Treschalin <mihail.treschalin@ya.ru>
2022-05-15 22:53:35 +0300

    7. Шаблон проектирования - Стратегия [#786]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,8 +2,8 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNull;
 
 public class StartUITest {
     @Test
