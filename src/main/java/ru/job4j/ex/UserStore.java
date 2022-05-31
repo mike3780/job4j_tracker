2a52247914e79f0f5f3a85d1caa42d51b5faf4be
Mihail Treschalin <mihail.treschalin@ya.ru>
2022-05-31 11:37:50 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #307246] - fix

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -6,6 +6,7 @@ public class UserStore {
         for (User usr : users) {
             if (login.equals(usr.getUsername())) {
                 user = usr;
+                break;
             }
         }
         if (user == null) {
@@ -15,7 +16,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid() | user.getUsername().length() < 3) {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("User not valid");
         }
         return true;
