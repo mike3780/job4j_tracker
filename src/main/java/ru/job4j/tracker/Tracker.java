1a01371a3d3ce7240b32843f2e9c75d04620c40e
Mihail Treschalin <mihail.treschalin@ya.ru>
2022-06-16 13:28:10 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039 #312917]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -30,7 +30,7 @@ public class Tracker {
     }
 
     public List<Item> findAll() {
-        return items;
+        return List.copyOf(items);
     }
 
     public List<Item> findByName(String key) {
@@ -48,8 +48,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             item.setId(id);
-            items.remove(index);
-            items.add(index, item);
+            items.set(index, item);
         }
         return rsl;
     }
