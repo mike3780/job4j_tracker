beb87efe88dac6a197e0b0c811092c044353fb74
Mihail Treschalin <mihail.treschalin@ya.ru>
2022-06-18 01:27:41 +0300

    2. Преобразования List в Map. [#10093 #313305]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,31 @@
+package ru.job4j.bank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+
+    }
+
+    public void addAccount(String passport, Account account) {
+
+    }
+
+    public User findByPassport(String passport) {
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
